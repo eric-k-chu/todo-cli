64604029edd0f3fd384c5489b129cb491900074d
eric-k-chu <kiyuen88ec@gmail.com>
2024-02-05 19:10:17 -0800

    testing program loop

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,15 +1,30 @@
 import { createInterface } from "readline";
+import { Option } from "./types.js";
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-async function main() {
-  console.log("todo-ts v1.0.0");
-  rl.question("Enter something: ", (input) => {
-    console.log(`You entered: ${input}`);
-    rl.close();
+function prompt(input?: Option) {
+  return `
+    todo-ts v1.0.0
+  
+    You currently have no todo items.
+  
+    [${input === "1" || !input ? "x" : " "}] Create a new Note
+
+    [${input === "0" ? "x" : " "}] Quit
+
+  `;
+}
+async function main(option?: Option) {
+  rl.question(prompt(option), (input) => {
+    if (input === "0") {
+      rl.close();
+    } else {
+      main();
+    }
   });
 }
 
