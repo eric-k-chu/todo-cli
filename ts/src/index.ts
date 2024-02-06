450b9306e60dff00a4be181d1f126ad0585bd1a6
eric-k-chu <kiyuen88ec@gmail.com>
2024-02-06 03:32:19 -0800

    renamed option to action

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -3,17 +3,17 @@ import { TodoAction, TodoActions } from "./types.js";
 
 console.log("TODO-TS v1.0.0");
 
-let option: TodoAction = "Create a todo";
+let action: TodoAction = "Create a todo";
 
 do {
   const answer = await inquirer.prompt([
     {
       type: "list",
-      name: "option",
+      name: "action",
       message: "Choose an action",
       choices: TodoActions,
     },
   ]);
-  if (answer.option !== "Exit") console.log(`\nYou chose: ${answer.option}\n`);
-  option = answer.option;
-} while (option !== "Exit");
+  if (answer.action !== "Exit") console.log(`\nYou chose: ${answer.action}\n`);
+  action = answer.action;
+} while (action !== "Exit");
