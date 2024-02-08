1356f2cb280ea3c00b82cb3a928ff7a472311b65
eric-k-chu <kiyuen88ec@gmail.com>
2024-02-08 00:26:43 -0800

    added create feature

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,11 +1,20 @@
-import { Operation } from "./types";
+import { readFile, writeFile } from "fs/promises";
+import { Operation, Todo, Todos } from "./types";
 
 try {
   const [, , op, ...args] = process.argv;
 
+  const json = await readFile("src/data.json", "utf-8");
+  const todos = JSON.parse(json) as Todos;
+
   switch (op as Operation) {
     case "create":
-      console.log("create");
+      const [todo] = args;
+      todos.todoList.push({
+        id: todos.nextId++,
+        isCompleted: false,
+        todo,
+      } as Todo);
       break;
     case "edit":
       console.log("edit");
@@ -21,6 +30,10 @@ try {
         "usage: npm run start <create | edit | update | delete> arg1 arg2"
       );
   }
+
+  const newJson = JSON.stringify(todos, null, 2);
+
+  if (json !== newJson) await writeFile("src/data.json", newJson);
 } catch (e) {
   console.error(
     e instanceof Error ? e.message : "An unknown error has occured."
--- ts/src/types.ts
+++ ts/src/types.ts
@@ -1,1 +1,12 @@
 export type Operation = "create" | "edit" | "update" | "delete";
+
+export interface Todo {
+  id: number;
+  isCompleted: boolean;
+  todo: string;
+}
+
+export interface Todos {
+  nextId: number;
+  todoList: Todo[];
+}
