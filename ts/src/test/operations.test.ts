9df7a8f9af979b7cccf6a1b7581065775a0bfbe6
eric-k-chu <kiyuen88ec@gmail.com>
2024-02-09 04:06:31 -0800

    added operation tests

--- ts/src/test/operations.test.ts
+++ ts/src/test/operations.test.ts
@@ -1,10 +1,60 @@
-import { viewTodo } from "../operations.js";
-import { Todo, Todos } from "../types.js";
+import {
+  createTodo,
+  deleteTodo,
+  editTodo,
+  finishTodo,
+  unfinishTodo,
+  viewTodo,
+} from "../operations.js";
+import { Todos } from "../types.js";
+
+describe("createTodo", () => {
+  it("create todo", () => {
+    const todo: Todos = {
+      nextId: 0,
+      todoList: {},
+    };
+    createTodo(todo, ["testing"]);
+    expect(todo).toEqual({
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: false,
+          todo: "testing",
+        },
+      },
+    });
+  });
+});
+
+describe("editTodo", () => {
+  it("edit todo", () => {
+    const todo: Todos = {
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: false,
+          todo: "te",
+        },
+      },
+    };
+    editTodo(todo, ["0", "testing"]);
+    expect(todo).toEqual({
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: false,
+          todo: "testing",
+        },
+      },
+    });
+  });
+});
 
 describe("viewTodo", () => {
   it("view one", () => {
     const todo: Todos = {
-      nextId: 1,
+      nextId: 2,
       todoList: {
         "1": {
           isCompleted: false,
@@ -14,6 +64,93 @@ describe("viewTodo", () => {
     };
     expect(viewTodo(todo, ["1"])).toEqual([
       { isCompleted: false, todo: "testing" },
-    ] as Todo[]);
+    ]);
+  });
+
+  it("view all", () => {
+    const todo: Todos = {
+      nextId: 3,
+      todoList: {
+        "1": {
+          isCompleted: false,
+          todo: "testing 1",
+        },
+        "2": {
+          isCompleted: false,
+          todo: "testing 2",
+        },
+      },
+    };
+    expect(viewTodo(todo, ["all"])).toEqual([
+      { isCompleted: false, todo: "testing 1" },
+      { isCompleted: false, todo: "testing 2" },
+    ]);
+  });
+});
+
+describe("finishTodo", () => {
+  it("finish a todo", () => {
+    const todo: Todos = {
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: false,
+          todo: "testing",
+        },
+      },
+    };
+    finishTodo(todo, ["0"]);
+    expect(todo).toEqual({
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: true,
+          todo: "testing",
+        },
+      },
+    });
+  });
+});
+
+describe("unfinishTodo", () => {
+  it("unfinish a todo", () => {
+    const todo: Todos = {
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: true,
+          todo: "testing",
+        },
+      },
+    };
+    unfinishTodo(todo, ["0"]);
+    expect(todo).toEqual({
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: false,
+          todo: "testing",
+        },
+      },
+    });
+  });
+});
+
+describe("deleteTodo", () => {
+  it("deleting a todo", () => {
+    const todo: Todos = {
+      nextId: 1,
+      todoList: {
+        "0": {
+          isCompleted: true,
+          todo: "testing",
+        },
+      },
+    };
+    deleteTodo(todo, ["0"]);
+    expect(todo).toEqual({
+      nextId: 1,
+      todoList: {},
+    });
   });
 });
