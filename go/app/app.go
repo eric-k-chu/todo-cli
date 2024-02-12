5a8b4c56acdc2cf3351d3cbbfac1e28e68e75223
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-11 20:36:20 -0800

    initial implementation of viewTodos

--- go/app/app.go
+++ go/app/app.go
@@ -7,23 +7,26 @@ import (
 )
 
 type Todo struct {
-	IsCompleted bool	`json:"isCompleted"`
-	Todo string				`json:"todo"`
+	IsCompleted bool   `json:"isCompleted"`
+	Todo        string `json:"todo"`
 }
 
 type Todos struct {
-	NextId int32	`json:"nextId"`
-	TodoList map[string]Todo	`json:"todoList"`
+	NextId   int32           `json:"nextId"`
+	TodoList map[string]Todo `json:"todoList"`
 }
 
+func viewTodos(todos Todos, args []string) {
+	for key, todo := range todos.TodoList {
+		fmt.Printf("Key: %s, isCompleted: %t, Todo: %s\n", key, todo.IsCompleted, todo.Todo)
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("args must be greater or equal to 2.")
 		return
 	}
-
-	fmt.Println("args:", os.Args[1])
 
 	file, err := os.Open("data.json")
 
@@ -42,10 +45,24 @@ func main() {
 		return
 	}
 
-	fmt.Println("NextId:", todos.NextId)
-	fmt.Println("TodoList:")
-	for key, todo := range todos.TodoList {
-		fmt.Printf("\tKey: %s, isCompleted: %t, Todo: %s\n", key, todo.IsCompleted, todo.Todo)
-	}
+	op := os.Args[1]
 
+	switch op {
+	case "view":
+		viewTodos(todos, os.Args)
+	case "create":
+		fmt.Println("Created.")
+	case "edit":
+		fmt.Println("Edited.")
+	case "update":
+		fmt.Println("Updated.")
+	case "delete":
+		fmt.Println("Deleted.")
+	case "finish":
+		fmt.Println("Finished.")
+	case "unfinish":
+		fmt.Println("Unfinished.")
+	default:
+		fmt.Println("operation does not match create, edit, update, delete, finish, or unfinish.")
+	}
 }
