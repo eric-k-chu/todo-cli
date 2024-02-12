5a0d97507fb5e720fde90bad385fc04ef98f23d2
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-11 19:57:31 -0800

    testing rapidjson

--- cpp/app.cpp
+++ cpp/app.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <stdio.h>
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+
+using namespace rapidjson;
 
 int main(int argc, char* argv[]) {
 	try
 	{
+		FILE* fp = fopen("data.json", "rb");
+		if (!fp) {
+			std::cerr << "Failed to open file." << std::endl;
+			return 1;
+		}
+
+		char buf[65536];
+		FileReadStream is(fp, buf, sizeof(buf));
+
+		fclose(fp);
+
+		Document doc;
+		doc.ParseStream(is);
+
+		if (doc.HasParseError()) {
+			std::cerr << "Parse error." << std::endl;
+			return 1;
+		}
+
+
+
 		if (argc < 3) throw 400;
 
 		for (int i = 0; i < argc; i++) {
