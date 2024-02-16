02b0ce8e8daee4b4c3a3d55d4ace8ae9b22850d4
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-15 23:36:44 -0800

    added create feature

--- cs/TodoApp/Program.cs
+++ cs/TodoApp/Program.cs
@@ -23,16 +23,54 @@ namespace TodoApp
             if (args.Length < 2)
             {
                 Console.WriteLine("Usage: <operation> <cmd>");
+                return;
             }
 
             // Reading from file
             string jsonTodos = File.ReadAllText(dataFile);
             Todos todos = JsonSerializer.Deserialize<Todos>(jsonTodos)!;
 
-            
+            string operation = args[0];
+
+            switch (operation)
+            {
+                case "create":
+                    CreateTodo(ref todos, args[1]);
+                    break;
+                case "view":
+                    Console.WriteLine("Create");
+                    break;
+                case "edit":
+                    Console.WriteLine("Create");
+                    break;
+                case "delete":
+                    Console.WriteLine("Create");
+                    break;
+                case "finish":
+                    Console.WriteLine("Create");
+                    break;
+                case "unfinish":
+                    Console.WriteLine("Create");
+                    break;
+                default:
+                    Console.WriteLine("Unknown operation. Must be create, view, edit, delete, finish, or unfinish");
+                    break;
+            }
+
             // Write to file
-            jsonTodos = JsonSerializer.Serialize(todos, new JsonSerializerOptions { WriteIndented = true});
+            jsonTodos = JsonSerializer.Serialize(todos, new JsonSerializerOptions { WriteIndented = true });
             File.WriteAllText(dataFile, jsonTodos);
+        }
+
+        static void CreateTodo(ref Todos todo, string todoStr)
+        {
+            Todo newTodo = new Todo
+            {
+                isCompleted = false,
+                todo = todoStr,
+            };
+            todo.todoList.Add(todo.nextId.ToString(), newTodo);
+            todo.nextId++;
         }
     }
 }
