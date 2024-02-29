8e9b84fe5325667db0137d45f2d26a0bcdd839ee
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-28 18:19:00 -0800

    testing reading args

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,7 +1,32 @@
-mod hello;
+use std::env;
 
 fn main() {
-    hello::print_hello();
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
 
-    hello::print_str("test");
+    if args.len() < 4 {
+        println!("Usage: cargo run . <op> <arg3> ...");
+        return;
+    }
+
+    let op: &String = &args[2];
+
+    if op == "create" {
+        println!("create");
+    } else if op == "view" {
+        println!("view");
+    } else if op == "edit" {
+        println!("edit");
+    } else if op == "edit" {
+        println!("delete");
+    } else if op == "edit" {
+        println!("edit");
+    } else if op == "finish" {
+        println!("finish");
+    } else if op == "unfinish" {
+        println!("unfinish");
+    } else {
+        println!("Unknown operation: Must be <create> <view> <edit> <delete> <finish> <unfinish>");
+        return;
+    }
 }
